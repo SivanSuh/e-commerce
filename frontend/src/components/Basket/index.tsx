cccd6b70480fe02ad63dc6cd6b91269f8aa7d6a0
sivansuh <sivansuh.is@gmail.com>
2023-08-22 22:49:06 +0300

    card effect animation added

--- frontend/src/components/Basket/index.tsx
+++ frontend/src/components/Basket/index.tsx
@@ -1,6 +1,7 @@
 import { RootState } from "@/store/store";
 import { useSelector } from "react-redux";
 import Style from "./style.module.css";
+import Button from "../Atoms/Button";
 
 const Basket = () => {
   const { cardItem } = useSelector((state: RootState) => state.basket);
@@ -8,11 +9,20 @@ const Basket = () => {
     <main className={Style.container}>
       {cardItem?.map((item) => (
         <div className={Style.basket} key={item._id}>
-          <p>{item.title}</p>
-          <figure>
-            <img src={item.image} alt={item.title} className="h-28" />
+          <p className="w-40">{item.title}</p>
+          <figure className="mx-5">
+            <img
+              src={item.image}
+              alt={item.title}
+              className="h-28 w-40 object-contain"
+            />
           </figure>
-          <p>{item.price}</p>
+          <p className="">{item.price}</p>
+          <div className={Style.counter}>
+            <Button title="-1" />
+            <span className="mx-3">{item.quantity}</span>
+            <Button title="+1" />
+          </div>
         </div>
       ))}
     </main>
--- frontend/src/components/Products/Card/CardComponents.tsx
+++ frontend/src/components/Products/Card/CardComponents.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Style from "./style.module.css";
 import CardProps from "./props";
 import Button from "@/components/Atoms/Button";
@@ -7,22 +7,28 @@ import { addBasket } from "@/store/slices/addBasketSlice";
 
 const CardComponents: React.FC<CardProps> = ({ item }) => {
   const dispatch = AppDispatch();
+  const [hover, setHover] = useState("none");
 
   const basket = () => {
     dispatch(addBasket({ ...item, quantity: 1 }));
   };
 
   return (
-    <div className={Style.card} key={item?._id}>
+    <div
+      className={Style.card}
+      key={item?._id}
+      onMouseEnter={() => setHover("flex")}
+      onMouseLeave={() => setHover("none")}
+    >
       <figure className={Style.figure}>
         <img src={item.image} alt={item.title} />
-        {/* <div className={Style.icon}>
-          <SlBasket size={30} />
-        </div> */}
       </figure>
       <h2 className={Style.title}>{item.title}</h2>
       <p className={Style.price}>{item.price}</p>
-      <div className="my-2 w-full">
+      <div
+        className="my-2 w-full ease-in-out duration-300"
+        style={{ display: hover }}
+      >
         <Button title="Add To Card" onClick={basket} bgColor="#fb8233" />
       </div>
     </div>
